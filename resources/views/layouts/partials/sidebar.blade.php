c1e1e8cc2edebb61bd75f9267f7cd9db8cb183c2
aldi-computer-home <codealdi@gmail.com>
2025-04-16 12:35:33 +0700

    pasang template dashboard

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,56 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="index.html" class="app-brand-link">
+            <span class="app-brand-logo demo">
+                
+            </span>
+            <span class="app-brand-text demo menu-text fw-bolder ms-2">ARC Fotocopy</span>
+        </a>
+
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+        <!-- Dashboard -->
+        <li class="menu-item active">
+            <a href="#" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div>Beranda</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="#" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div>Manajemen Barang</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="#" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div>Manajemen Jasa</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-detail"></i>
+                <div >Manajemen Pesanan</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="forms-basic-inputs.html" class="menu-link">
+                        <div>Barang</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="forms-input-groups.html" class="menu-link">
+                        <div>Jasa</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
