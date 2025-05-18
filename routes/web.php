c75083293c23860c69cb8486147c572cc4ba5984
CodeAldi <codealdi@gmail.com>
2025-05-18 20:41:41 +0700

    membagi route untuk admin dan pembeli

--- routes/web.php
+++ routes/web.php
@@ -36,24 +36,27 @@ Route::controller(authenticateController::class)->group(function() {
     Route::post('/logout','logoutAksi')->middleware('auth')->name('logout.aksi');
 });
 
-Route::controller(BarangController::class)->group(function(){
-    Route::get('/barang','index')->name('barang.index');
-    Route::post('/barang/tambah', 'store')->name('barang.tambah');
-    Route::post('/barang/hapus', 'destroy')->name('barang.hapus');
-});
-Route::controller(KategoriBarangController::class)->group(function(){
-    Route::post('/kategori-barang/tambah','store')->name('kategoriBarang.tambah');
-    Route::post('/kategori-barang/update','update')->name('kategoriBarang.update');
-    Route::post('/kategori-barang/hapus','destroy')->name('kategoriBarang.hapus');
-});
-Route::controller(JasaController::class)->group(function(){
-    Route::get('jasa','index')->name('jasa.index');
-    Route::post('jasa/tambah','store')->name('jasa.tambah');
-    Route::post('jasa/edit','update')->name('jasa.edit');
-    Route::post('jasa/hapus','destroy')->name('jasa.hapus');
-});
-Route::controller(KategoriJasaController::class)->group(function(){
-    Route::post('kategori-jasa/tambah','store')->name('kategoriJasa.tambah');
-    Route::post('kategori-jasa/update','update')->name('kategoriJasa.update');
-    Route::post('kategori-jasa/hapus','destroy')->name('kategoriJasa.hapus');
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::controller(BarangController::class)->group(function(){
+        Route::get('/barang','index')->name('barang.index');
+        Route::post('/barang/tambah', 'store')->name('barang.tambah');
+        Route::post('/barang/hapus', 'destroy')->name('barang.hapus');
+    });
+    Route::controller(KategoriBarangController::class)->group(function(){
+        Route::post('/kategori-barang/tambah','store')->name('kategoriBarang.tambah');
+        Route::post('/kategori-barang/update','update')->name('kategoriBarang.update');
+        Route::post('/kategori-barang/hapus','destroy')->name('kategoriBarang.hapus');
+    });
+    Route::controller(JasaController::class)->group(function(){
+        Route::get('jasa','index')->name('jasa.index');
+        Route::post('jasa/tambah','store')->name('jasa.tambah');
+        Route::post('jasa/edit','update')->name('jasa.edit');
+        Route::post('jasa/hapus','destroy')->name('jasa.hapus');
+    });
+    Route::controller(KategoriJasaController::class)->group(function(){
+        Route::post('kategori-jasa/tambah','store')->name('kategoriJasa.tambah');
+        Route::post('kategori-jasa/update','update')->name('kategoriJasa.update');
+        Route::post('kategori-jasa/hapus','destroy')->name('kategoriJasa.hapus');
+    });
+
 });
